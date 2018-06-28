bde401ea8e9e74036f7c90e776e557179a9638f9
Jamie Brown <jmbrowncow@gmail.com>
2018-06-28 22:01:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.count = 2
+  if array.length = 2
     "#{array[0]} and #{array[1]}"
-  elsif array.count = 1
+  elsif array.length = 1
     array[0]
   else
     new_entry = "and #{array[last]}"
